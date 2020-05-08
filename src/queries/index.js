577206a1a82af3e03c1a8276f939a22ddc9a9cfe
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-05-09 01:45:41 +0900

    mutationの書き方を合わせた

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,9 +1,7 @@
 import {gql} from '@apollo/client';
 
 export const LOGIN = gql`
-  mutation Login($username: String!, $password: String!) {
-    login(username: $username, password: $password) {
-      token
-    }
+  mutation Login($loginInput: LoginInput!){
+    login(input: $loginInput) 
   }
 `;
