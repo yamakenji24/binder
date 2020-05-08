4243ef048e7161cb1b4352a5150e7e7e3e171c7e
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-05-09 01:46:41 +0900

    定数関連は/constantsに

--- src/index.js
+++ src/index.js
@@ -1,33 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {ApolloClient, ApolloProvider, createHttpLink, InMemoryCache} from '@apollo/client';
-import {setContext} from '@apollo/link-context';
+import {ApolloProvider} from '@apollo/client';
 import Login from './components/Login';
+import * as apollo from './constants/apollo';
 import * as serviceWorker from './serviceWorker';
 import './stylesheets/index.css';
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:8080/query',
-});
-
-const authLink = setContext((_, {headers}) => {
-  const token = localStorage.getItem("token")
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
-  }
-})
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-  credentials: 'include'
-})
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apollo.client}>
       <Login />
     </ApolloProvider>
   </React.StrictMode>,
