db64b133607df919f645e2193c905ec7e7f41a6c
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-05-09 00:04:12 +0900

    mutation for graphql

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './Login';
+import {ApolloClient, ApolloProvider, createHttpLink, InMemoryCache} from '@apollo/client';
+import {setContext} from '@apollo/link-context';
+import Login from './components/Login';
 import * as serviceWorker from './serviceWorker';
 import './stylesheets/index.css';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8080/query',
+});
+
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem("token")
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+})
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  credentials: 'include'
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <ApolloProvider client={client}>
+      <Login />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
