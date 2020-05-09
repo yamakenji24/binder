3a60e841bb1836f75438c4ce296c1b26e8d00668
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-05-10 07:03:15 +0900

    styles for logout modal

---
+++ src/constants/modal.js
@@ -0,0 +1,15 @@
+
+export const logoutModal = {
+  overlay: {
+    zIndex: 100,
+    backgroundColor: "rgba(0, 0, 0, 0.5)"
+  },
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+}
