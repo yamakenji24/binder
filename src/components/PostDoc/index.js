8d2768932327b3d65e394f378f4314e2f6fe3514
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-05-23 01:09:45 +0900

    add title and tag to post

--- src/components/PostDoc/index.js
+++ src/components/PostDoc/index.js
@@ -1,28 +1,45 @@
 import React, {useState, useRef} from 'react';
 import NaviBar from '../TopPage/NaviBar';
-import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import { Document, Page, pdfjs} from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function PostDoc() {
+  // この辺り整理が必要
   const [numPages, setNumPage] = useState(null)
   const [page, setPage] = useState(1)
   const [base64, setBase] = useState(null)
   const [name, setName] = useState(null)
   const fileInput = useRef()
+  const titleInput = useRef()
+  const tagInput = useRef()
   
   const handleChange = () => {
     const file = fileInput.current.files[0]
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onload = () => {
-      setBase(reader.result)
-      setName(file.name)
+       
+    if (file) {
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onload = () => {
+        setBase(reader.result)
+        setName(file.name)
+      }
     }
   }
-
+  
+  const submitPostData = () => {
+    // DB に登録したい
+    const title = titleInput.current._valueTracker.getValue()
+    const tag = tagInput.current._valueTracker.getValue().split(' ')
+    const file = fileInput.current.files[0]
+    if (!title || !tag || !file) {
+      console.log("failed")
+    } else {
+      console.log("success")
+    }
+  }
   const onDocumentLoadSuccess = ({numPages}) =>{
     setNumPage(numPages)
   }
@@ -36,8 +53,8 @@ export default function PostDoc() {
       <h2>Document? の投稿ページ</h2>
       <Form>
         <Form.Group>
-          <FormControl placeholder="Title" aria-label="Title" />
-          <FormControl placeholder="関連するタグを？個まで登録できまっせ" aria-label="tag" />
+          <FormControl ref={titleInput} placeholder="Title" aria-label="Title" />
+          <FormControl ref={tagInput} placeholder="関連するタグを？個まで登録できまっせ" aria-label="tag" />
         </Form.Group>
       </Form>
       <div className='pdfcomponent'>
@@ -60,7 +77,9 @@ export default function PostDoc() {
         <button disabled={page >= numPages || !numPages} onClick={() => handleButtonClick(page + 1)}>
           Next
         </button>
-        
+        <br /> <br />
+
+        <Button variant="success" onClick={submitPostData}>投稿する</Button>
       </div>
     </div>
   )
