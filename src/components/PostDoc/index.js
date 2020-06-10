58d971c13c4ad166d9a24ef57de67d4932543bd0
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-06-10 21:13:35 +0900

    implement graphql of create document

--- src/components/PostDoc/index.js
+++ src/components/PostDoc/index.js
@@ -4,6 +4,8 @@ import FormControl from 'react-bootstrap/FormControl';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Document, Page, pdfjs} from 'react-pdf';
+import {useMutation} from '@apollo/client';
+import * as Graphql from '../../graphql';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function PostDoc() {
@@ -12,10 +14,21 @@ export default function PostDoc() {
   const [page, setPage] = useState(1)
   const [base64, setBase] = useState(null)
   const [name, setName] = useState(null)
+  const [state, setState] = useState({
+    titleInput: '',
+    tagInput: '',
+    description: ''
+  })
   const fileInput = useRef()
-  const titleInput = useRef()
-  const tagInput = useRef()
-  
+  const [createDocument, {data, error}] = useMutation(Graphql.CREATEDOCUMENT, {
+    onCompleted({createDocument}) {
+      console.log(createDocument)
+    },
+    onError({error}) {
+      console.log(error)
+    }
+  })
+    
   const handleChange = () => {
     const file = fileInput.current.files[0]
     
@@ -31,13 +44,20 @@ export default function PostDoc() {
   
   const submitPostData = () => {
     // DB に登録したい
-    const title = titleInput.current._valueTracker.getValue()
-    const tag = tagInput.current._valueTracker.getValue().split(' ')
-    const file = fileInput.current.files[0]
-    if (!title || !tag || !file) {
+    const tag = state.tagInput.split(' ')
+    if (!state.titleInput || !tag || !base64) {
       console.log("failed")
     } else {
-      console.log("success")
+      console.log(state.titleInput, state.description, base64)
+      createDocument({
+        variables: {
+          documentInput: {
+            "title": state.titleInput,
+            "description": state.description,
+            "file": base64.split(',')[1],
+          }
+        }
+      })
     }
   }
   const onDocumentLoadSuccess = ({numPages}) =>{
@@ -46,6 +66,10 @@ export default function PostDoc() {
   const handleButtonClick = (page) =>{
     setPage(page)
   }
+
+  const onChange = event => {
+    setState({...state, [event.target.name]: event.target.value})
+  }
   
   return (
     <div>
@@ -53,8 +77,9 @@ export default function PostDoc() {
       <h2>Document? の投稿ページ</h2>
       <Form>
         <Form.Group>
-          <FormControl ref={titleInput} placeholder="Title" aria-label="Title" />
-          <FormControl ref={tagInput} placeholder="関連するタグを？個まで登録できまっせ" aria-label="tag" />
+          <FormControl onChange={onChange} value={state.titleInput} name="titleInput" placeholder="Title" />
+          <FormControl onChange={onChange} value={state.tagInput} name="tagInput" placeholder="関連するタグを？個まで登録できまっせ" />
+          <FormControl onChange={onChange} value={state.description} name="description" placeholder="概要" type="text" />
         </Form.Group>
       </Form>
       <div className='pdfcomponent'>
