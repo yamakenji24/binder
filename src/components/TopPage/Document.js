2bb9297f03b801cd574d44600d04eb4b2be9b954
Reo Ishii <leopon.owl@gamil.com>
2020-07-15 23:53:06 +0900

    render pdf from minio in toppage

--- src/components/TopPage/Document.js
+++ src/components/TopPage/Document.js
@@ -1,9 +1,40 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import PDFDocument from "../Documents/Document";
+import { GET_DOCUMENTS } from "../../graphql";
+import { useQuery } from "@apollo/client";
 
 export default function Doc() {
-  return (
-    <div>
-      <h3>showing documentation</h3>
-    </div>
-  );
+    const { loading, error, data } = useQuery(GET_DOCUMENTS, {
+        variables: {
+            paginationInput: {
+                first: 3,
+                offset: 0,
+            },
+        },
+    });
+
+    if (loading) return <Container><h1>loading...</h1></Container>;
+    if (error) return <Container><h1>error!</h1></Container>;
+
+    return (
+        <div>
+            <h3>showing documentation</h3>
+            <Container>
+                {
+                    data.docs.edges.map(edge => {
+                        return(
+                            <Row key={edge.cursor}>
+                                <Col xs={12} md={6}>
+                                    <PDFDocument file={edge.node.file} title={edge.node.title} />
+                                </Col>
+                            </Row>
+                        );
+                    })
+                }
+            </Container>
+        </div>
+    );
 }
